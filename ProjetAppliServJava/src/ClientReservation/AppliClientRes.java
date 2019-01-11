7d724a3ee85d364990d5eb10292584f26df3ac89
Hestia9 <46046413+Hestia9@users.noreply.github.com>
2019-01-11 12:51:14 +0100

    Add files via upload

---
+++ ProjetAppliServJava/src/ClientReservation/AppliClientRes.java
@@ -0,0 +1,12 @@
+package ClientReservation;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class AppliClientRes {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+		ClientReservation cli = new ClientReservation();
+		cli.reserver();
+	}
+}
