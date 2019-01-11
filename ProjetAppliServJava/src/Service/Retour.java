bb9bc374900c3f0ef0603668222fa3661038acaa
Deuteroos <46046408+Deuteroos@users.noreply.github.com>
2019-01-11 12:57:38 +0100

    Add files via upload

--- ProjetAppliServJava/src/Service/Retour.java
+++ ProjetAppliServJava/src/Service/Retour.java
@@ -1,6 +1,13 @@
 package Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+
+import Abonne.Abonne;
+import Document.Document;
 
 public class Retour extends Service{
 
@@ -12,7 +19,45 @@ public class Retour extends Service{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("*********Connexion "+this.getClass()+" démarrée");
+		System.out.println("*********Connexion "+this.getClient().getInetAddress());
+		try {
+			BufferedReader in = new BufferedReader (new InputStreamReader(getClient().getInputStream ( )));
+			PrintWriter out = new PrintWriter (getClient().getOutputStream ( ), true);
+			Abonne a = null;
+			while(a == null) {
+				String line = in.readLine();
+				int numAbo = Integer.parseInt(line);
+				try {
+					a = getBaseAbo().getAboByNum(numAbo);					
+				}
+				catch(IllegalArgumentException i){
+					out.print("Erreur de connexion, l'identification a échoué !");
+				}
+			}
+			out.println("Connexion réussie !");
+			Document d = null;
+			while(d == null) {
+				String line = in.readLine();
+				int numDoc = Integer.parseInt(line);
+				try {
+					d = getBiblio().getDocByNum(numDoc);	
+					synchronized (d) {
+						d.retour();
+					}
+					out.println("Le document a été retourné avec succès !");
+				}
+				catch(IllegalArgumentException i){
+					out.print("Erreur, le document demandé n'existe pas !");
+				}
+			}
+			
+		}
+		catch (IOException e) {
+		}
+		//Fin du service d'emprunt
+		System.out.println("*********Connexion "+this.getClass()+" terminée");
+		try {getClient().close();} catch (IOException e2) {}
 	}
 
 }
