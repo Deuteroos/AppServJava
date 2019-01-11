6b7f7e917213f6605be232fb58c8d15243bf251f
Deuteroos <46046408+Deuteroos@users.noreply.github.com>
2019-01-11 12:56:04 +0100

    Add files via upload

---
+++ ProjetAppliServJava/src/ClientRetour/AppliClientR.java
@@ -0,0 +1,14 @@
+package ClientRetour;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+
+
+public class AppliClientR {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+		ClientRetour cli = new ClientRetour();
+		cli.retour();
+	}
+}
